fe438945c6e52e941fa2cdefd1661706a8319106
fern1999 <fvillegas@playwire.com>
2022-06-15 15:02:59 -0500

    updated

--- tutorial1_change.py
+++ tutorial1_change.py
@@ -6,9 +6,10 @@ database = "google_analytics"
 bucket = "playwire-analytics"
 athena = AthenaDAO(region, database, bucket)
 
-que =  "SELECT * FROM ga_ramp_daily"
+que =  "SELECT * FROM ga_ramp_daily limit 10"
 athena = AthenaDAO(region, database, bucket)
 rr = athena.execute(query=que, download=True)
+print(rr.head())
 
 ####
 # Create a string Query that queries the ga_ramp_daily table in the google
