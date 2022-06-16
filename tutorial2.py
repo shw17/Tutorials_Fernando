473d2bc26ec96bbccefb0542bb57d5fc0245c352
fern1999 <fvillegas@playwire.com>
2022-06-16 08:10:10 -0500

    second tutorial

---
+++ tutorial2.py
@@ -0,0 +1,9 @@
+# Import necessary class
+
+# Make a query that answers the following question: what is the average number of users, sessions per country, per date ?
+
+# Download results to your script
+
+# Convert resulting data frame to a csv file
+
+# next we will upload csv file to s3, but let me know when you get to this step
