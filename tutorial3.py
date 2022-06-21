a8cdc02c8c4459e9de04ed7d80709eab316d1860
fern1999 <fvillegas@playwire.com>
2022-06-21 16:01:25 +0200

    tutorial 3

---
+++ tutorial3.py
@@ -0,0 +1,16 @@
+# Import necessary class
+
+
+# Learn how to iterature through dates using datetime function
+
+# iterate through the last 7 days. Examples can be seen here: https://www.geeksforgeeks.org/python-iterating-through-a-range-of-dates/
+    # in the inner loop, make a dynamic query that finds the average number of ad_requests, impressions,revenue for each site for each date
+        # use the table: triplelift.triplelift
+
+# Convert resulting data frame to a csv file
+# You should have a csv file for the past 7 days - one for each day
+
+# next we will upload csv file to s3, but let me know when you get to this step
+
+# upload csv file to the following s3 path: s3://playwire-analytics/shuwen-dev/tutorial2/
+
