60f04cd3e3aad1dcebc67c35825379dd37219756
shw17 <89555673+shw17@users.noreply.github.com>
2022-06-23 12:13:47 -0400

    Update pyspark_install.py

--- pyspark_install.py
+++ pyspark_install.py
@@ -1,10 +1,13 @@
 # in the terminal: pip install pyspark 
 
 import pyspark
+from pyspark import SparkFiles
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('Tutorial3').getOrCreate()
-df = spark.read.csv('spam_base.csv',header=True)
+spark = SparkSession.builder.appName('installpy').getOrCreate()
+url = "https://raw.githubusercontent.com/shw17/Tutorials_Fernando/master/spam_base.csv"
+spark.sparkContent.addFile(url)
+df = spark.read.csv(SparkFiles.get("spam_base.csv"),inferSchema=True, header=True)
 print(type(df))
 print(df.head())
 print(df.printSchema())
